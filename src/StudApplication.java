9ba73181370826765a1407200cfa7a17f95d7080
Dmytro <mr.dima.zhuravlov@gmail.com>
2022-09-12 23:06:59 +0200

    Initial commit

--- src/StudApplication.java
+++ src/StudApplication.java
@@ -14,14 +14,14 @@ public class StudApplication {
             students[i] = new Student();
             System.out.println("Enter student name");
             scanner.nextLine();
-            students[i].name = scanner.nextLine();
+            students[i].setName(scanner.nextLine());
             System.out.println("Enter student lastname");
-            students[i].surname = scanner.nextLine();
+            students[i].setSurname(scanner.nextLine());
             System.out.println("Enter student year of study");
-            students[i].yearOfStudy = scanner.nextInt();
+            students[i].setYearOfStudy(scanner.nextInt());
             System.out.println("Enter student balance");
-            students[i].balance = scanner.nextInt();
-            startBalance = students[i].balance;
+            students[i].setBalance(scanner.nextInt());
+            startBalance = students[i].getBalance();
             System.out.println("Student class levels:");
             System.out.println("""
                     1 - Freshmen
@@ -29,11 +29,20 @@ public class StudApplication {
                     3 - Junior
                     4 - Middle""");
             System.out.println("Enter student class level (number)");
-            int studLvl = scanner.nextInt();
-            students[i].indicator = studLvl * 1000 + i;
-            System.out.println(students[i].name + " " + students[i].surname + " "
-                    + studLvl + " " + students[i].indicator);
-            if(students[i].balance > 600) {
+            int studLvl;
+            while (true) {
+                studLvl = scanner.nextInt();
+                if(studLvl < 1 || studLvl > 4) {
+                    System.out.println("Such student class not exist");
+                } else {
+                    break;
+                }
+
+            }
+            students[i].setIndicator(studLvl * 1000 + i);
+            System.out.println(students[i].getName() + " " + students[i].getSurname() + " "
+                    + studLvl + " " + students[i].getIndicator());
+            if(students[i].getBalance() > 600) {
                 System.out.println("Enter the desired subject or q to to quit");
                 System.out.println("""
                         1) History
@@ -43,17 +52,19 @@ public class StudApplication {
                 scanner.nextLine();
                 String desiredSubject = scanner.nextLine();
                 while(true) {
-                    if(students[i].balance < 600) {
-                        System.out.println("You don't have enough money to buy course"
-                                + "\nEnter \"q\" to quit");
-                    }
-                    if(desiredSubject.equals("History") || desiredSubject.equals("Mathematics")
-                    || desiredSubject.equals("Chemistry") || desiredSubject.equals("Informatics")) {
-                        students[i].balance -= 600;
-                        payment += 600;
-                        desiredSubject = scanner.nextLine();
+                    if(students[i].getBalance() > 600) {
+                        if(desiredSubject.equals("History") || desiredSubject.equals("Mathematics")
+                                || desiredSubject.equals("Chemistry") || desiredSubject.equals("Informatics")) {
+                            students[i].setBalance(students[i].getBalance() - 600); ;
+                            payment += 600;
+                            desiredSubject = scanner.nextLine();
+                        } else {
+                            System.out.println("Such an subject does not exist");
+                            desiredSubject = scanner.nextLine();
+                        }
                     } else {
-                        System.out.println("Such an subject does not exist");
+                        System.out.println("You don't have money on any course"
+                                + "\nEnter q to quit");
                         desiredSubject = scanner.nextLine();
                     }
                     if(desiredSubject.equals("q") || desiredSubject.equals("Q")) {
@@ -62,7 +73,7 @@ public class StudApplication {
                 }
                 System.out.println("Your balance: " + startBalance);
                 System.out.println("You pay: " + payment);
-                System.out.println("Remained on the account: " + students[i].balance
+                System.out.println("Remained on the account: " + students[i].getBalance()
                         + "\nThank you for payment");
                 System.out.println();
             }
--- src/Student.java
+++ src/Student.java
@@ -1,8 +1,48 @@
 
 public class Student {
-    String name;
-    String surname;
-    int indicator;
-    int yearOfStudy;
-    int balance;
+    private String name;
+    private String surname;
+    private int indicator;
+    private int yearOfStudy;
+    private int balance;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getIndicator() {
+        return indicator;
+    }
+
+    public void setIndicator(int indicator) {
+        this.indicator = indicator;
+    }
+
+    public int getYearOfStudy() {
+        return yearOfStudy;
+    }
+
+    public void setYearOfStudy(int yearOfStudy) {
+        this.yearOfStudy = yearOfStudy;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
 }
